333eecb4240bc6d73ce0a25b5f5c58d1267caef7
Jakub Krąpiec <266503@student.pwr.edu.pl>
2024-01-15 20:21:28 +0100

    Update client.py

--- client/client.py
+++ client/client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from http import server
+from typing import Tuple
 from config import *
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
@@ -20,55 +20,68 @@ routes: list[Route] = []
 current_stop_index: int = None
 client = mqtt.Client()
 stop_rfid_polling = False
-last_card_scan_value_time: (int, datetime.datetime) = (None, None)
+last_card_scan_value_time: Tuple[int, datetime.datetime] = (None, None)
+last_encoder_event: datetime.datetime = None
 
 
-def on_encoder_left_while_selecting_route() -> None:
+def on_encoder_left_while_selecting_route(_) -> None:
     global route_index
     global routes
+    global last_encoder_event
+    if last_encoder_event is not None and last_encoder_event + datetime.timedelta(seconds=0.5) > datetime.datetime.now():
+        return
+    last_encoder_event = datetime.datetime.now()
     if route_index > 0:
         route_index -= 1
         draw_routes_menu(routes, route_index)
 
-def on_encoder_right_while_selecting_route() -> None:
+def on_encoder_right_while_selecting_route(_) -> None:
     global route_index
     global routes
+    global last_encoder_event
+    if last_encoder_event is not None and last_encoder_event + datetime.timedelta(seconds=0.5) > datetime.datetime.now():
+        return
+    last_encoder_event = datetime.datetime.now()
     if route_index < len(routes) - 1:
         route_index += 1
         draw_routes_menu(routes, route_index)
 
-def on_green_button_while_on_route() -> None:
+def on_green_button_while_on_route(_) -> None:
     global current_stop_index
     global routes
     global route_index
+    global stop_rfid_polling
+
     route = routes[route_index]
     if current_stop_index < len(route.stops) - 1:
         current_stop_index += 1
         draw_stops_screen(route, current_stop_index)
         client.publish("worker/notify", f"NEXT;{routes[route_index].name};{current_stop_index}")
     else:
-        GPIO.remove_event_detect(greenButton)
-        GPIO.remove_event_detect(redButton)
+        stop_rfid_polling = True
+        GPIO.remove_event_detect(buttonGreen)
+        GPIO.remove_event_detect(buttonRed)
         client.publish("worker/notify", f"FINISH;{routes[route_index].name}")
         route_index = 0
         select_route()
+        
 
-def on_red_button_while_on_route() -> None:
+def on_red_button_while_on_route(_) -> None:
     global current_stop_index
     global route_index
     global stop_rfid_polling
-    GPIO.remove_event_detect(greenButton)
-    GPIO.remove_event_detect(redButton)
+    GPIO.remove_event_detect(buttonGreen)
+    GPIO.remove_event_detect(buttonRed)
     route_index = 0
     stop_rfid_polling = True
     current_stop_index = None
     client.publish("worker/notify", f"END;{routes[route_index].name}")
     select_route()
 
-def on_green_pressed_while_selecting_route() -> None:
+def on_green_pressed_while_selecting_route(_) -> None:
     GPIO.remove_event_detect(encoderLeft)
     GPIO.remove_event_detect(encoderRight)
-    GPIO.remove_event_detect(greenButton)
+    GPIO.remove_event_detect(buttonGreen)
     begin_route()
 
 def on_card_scanned(uid: int) -> None:
@@ -76,6 +89,7 @@ def on_card_scanned(uid: int) -> None:
     global current_stop_index
     global routes
     global route_index
+    print(f'scanned {uid}')
     (last_value, last_time) = last_card_scan_value_time
     if last_time is not None and last_time + datetime.timedelta(seconds=7) < datetime.datetime.now():
         return
@@ -84,6 +98,7 @@ def on_card_scanned(uid: int) -> None:
 
 def listen_rfid() -> None:
     global stop_rfid_polling
+    MIFAREReader = MFRC522()
     while True:
         if stop_rfid_polling:
             stop_rfid_polling = False
@@ -94,15 +109,15 @@ def listen_rfid() -> None:
             if status == MIFAREReader.MI_OK:
                 on_card_scanned(uid)
         # this sleep is okay, since this function runs on a separate thread
-        time.sleep(0.5)
-    sys.exit()
+        time.sleep(1)
+    #sys.exit()
 
 def select_route() -> Route:
     global routes
     global route_index
-    GPIO.add_event_detect(encoderLeft, GPIO.FALLING, callback=on_encoder_left_while_selecting_route, bouncetime=100)
-    GPIO.add_event_detect(encoderRight, GPIO.FALLING, callback=on_encoder_right_while_selecting_route, bouncetime=100)
-    GPIO.add_event_detect(greenButton, GPIO.FALLING, callback=on_green_pressed_while_selecting_route, bouncetime=100)
+    GPIO.add_event_detect(encoderLeft, GPIO.FALLING, callback=on_encoder_left_while_selecting_route, bouncetime=500)
+    GPIO.add_event_detect(encoderRight, GPIO.FALLING, callback=on_encoder_right_while_selecting_route, bouncetime=500)
+    GPIO.add_event_detect(buttonGreen, GPIO.FALLING, callback=on_green_pressed_while_selecting_route, bouncetime=500)
     draw_routes_menu(routes, route_index)
 
 
@@ -112,8 +127,8 @@ def begin_route() -> None:
     global routes
     global route_index
     current_stop_index = 0
-    GPIO.add_event_detect(greenButton, GPIO.FALLING, callback=on_green_button_while_on_route, bouncetime=100)
-    GPIO.add_event_detect(redButton, GPIO.FALLING, callback=on_red_button_while_on_route, bouncetime=100)
+    GPIO.add_event_detect(buttonGreen, GPIO.FALLING, callback=on_green_button_while_on_route, bouncetime=500)
+    GPIO.add_event_detect(buttonRed, GPIO.FALLING, callback=on_red_button_while_on_route, bouncetime=500)
     client.publish("worker/notify", f"START;{routes[route_index].name}")
     draw_stops_screen(routes[route_index], current_stop_index)
     stop_rfid_polling = False
@@ -148,3 +163,5 @@ if __name__ == "__main__":
     route4 = Route("Autobus 169", [])
     routes = [route1, route2, route3, route4]
     select_route()
+    while True:
+        _ = input()
